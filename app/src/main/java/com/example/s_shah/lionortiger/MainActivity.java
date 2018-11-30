63379a21bdd0878a7dd61a5218157533717ae7e7
S-Shah <almaasshah>
2018-11-30 08:24:22 +0000

    No Winner Bug Fix.

--- app/src/main/java/com/example/s_shah/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/s_shah/lionortiger/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean gameOver = false;
     private Button reset_btn;
     private android.support.v7.widget.GridLayout gridLayout;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         reset_btn = findViewById(R.id.reset_btn);
         gridLayout = findViewById(R.id.gridLayout);
+        counter = 0;
 
         reset_btn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -48,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView tappedImageView = (ImageView) imageView;
         int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
 
+
+        if (counter == playerChoices.length){reset_btn.setVisibility(View.VISIBLE);
+        gameOver = true;
+        Toast.makeText(this, "It's a draw!", Toast.LENGTH_LONG).show();
+        }
+
         if(playerChoices[tiTag]==Player.No && gameOver == false) {
             tappedImageView.setTranslationX(-2000);
             playerChoices[tiTag] = currentPlayer;
@@ -55,9 +63,11 @@ public class MainActivity extends AppCompatActivity {
             if (currentPlayer == Player.ONE) {
                 tappedImageView.setImageResource(R.drawable.lion);
                 currentPlayer = Player.TWO;
+                counter+=1;
             } else if (currentPlayer == Player.TWO) {
                 tappedImageView.setImageResource(R.drawable.tiger);
                 currentPlayer = Player.ONE;
+                counter+=1;
             }
             tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
             // Toast.makeText(this, tappedImageView.getTag().toString(), Toast.LENGTH_SHORT).show();
@@ -94,8 +104,9 @@ public class MainActivity extends AppCompatActivity {
         for (int index = 0; index <playerChoices.length; index++){
             playerChoices[index] = Player.No;
         }
-        
+
         gameOver = false;
+        counter = 0;
         reset_btn.setVisibility(View.GONE);
     }
 }
